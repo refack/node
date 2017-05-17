a258e1ccc37cb70aac719dd239fcbc1343988bea
Sebastian Plesciuc <sebastian.plesciuc@sendgrid.com>
2017-05-17 07:42:15 +0300

    test: fix sequential test-net-connect-local-error
    Fixed sequential test-net-connect-local-error by swapping port
    and localPort in net.connect options.
    
    Fixes: https://github.com/nodejs/node/issues/13055

--- test/sequential/test-net-connect-local-error.js
+++ test/sequential/test-net-connect-local-error.js
@@ -4,24 +4,16 @@ const assert = require('assert');
 const net = require('net');
 
 const client = net.connect({
-  port: common.PORT + 1,
-  localPort: common.PORT,
+  port: common.PORT,
+  localPort: common.PORT + 1,
   localAddress: common.localhostIPv4
 });
 
 client.on('error', common.mustCall(function onError(err) {
   assert.strictEqual(err.syscall, 'connect');
   assert.strictEqual(err.code, 'ECONNREFUSED');
-  assert.strictEqual(
-    err.localPort,
-    common.PORT,
-    `${err.localPort} !== ${common.PORT} in ${err}`
-  );
-  assert.strictEqual(
-    err.localAddress,
-    common.localhostIPv4,
-    `${err.localAddress} !== ${common.localhostIPv4} in ${err}`
-  );
+  assert.strictEqual(err.localPort, common.PORT + 1);
+  assert.strictEqual(err.localAddress, common.localhostIPv4);
   assert.strictEqual(
     err.message,
     `connect ECONNREFUSED ${err.address}:${err.port} ` +
