aca2b13604621e36a21da5ff3f5d91dbea39d8a3
Refael Ackermann <refack@gmail.com>
2017-06-05 10:13:09 -0400

    all test cases suggested by not-an-aardvark

--- test/parallel/test-util-callbackify.js
+++ test/parallel/test-util-callbackify.js
@@ -1,52 +1,154 @@
 'use strict';
 const common = require('../common');
 
-
 const assert = require('assert');
 const { callbackify } = require('util');
 const { join } = require('path');
 const { execFile } = require('child_process');
 const fixtureDir = join(common.fixturesDir, 'uncaught-exceptions');
+const sentinalValues = [
+  'hello world',
+  null,
+  undefined,
+  { key: 'value' },
+  Symbol('I am a symbol'),
+  function ok() {},
+  ['array', 'with', 4, 'values'],
+  new Error('boo')
+];
 
 {
-  const sentinel = 'hello world';
-  async function fn() {
-    return await Promise.resolve(sentinel);
-  }
-  const cbFn = callbackify(fn);
+  // Just works
+  for (const sentinel of sentinalValues) {
+    async function asyncFn() {
+      return await Promise.resolve(sentinel);
+    }
 
-  cbFn(common.mustCall((err, ret) => {
-    assert.ifError(err);
-    assert.strictEqual(ret, sentinel);
-  }));
+    const cbAsyncFn = callbackify(asyncFn);
+
+    cbAsyncFn(common.mustCall((err, ret) => {
+      assert.ifError(err);
+      assert.strictEqual(ret, sentinel);
+    }));
+
+    function promiseFn() {
+      return Promise.resolve(sentinel);
+    }
+
+    const cbPromiseFn = callbackify(promiseFn);
+
+    cbPromiseFn(common.mustCall((err, ret) => {
+      assert.ifError(err);
+      assert.strictEqual(ret, sentinel);
+    }));
+  }
 }
 
 {
-  const sentinel = 'hello world 2';
-  function fn2() {
-    return Promise.resolve(sentinel);
-  }
-  const cbFn = callbackify(fn2);
+  // Rejections
+  for (const sentinel of sentinalValues) {
+    async function asyncFn() {
+      return await Promise.reject(sentinel);
+    }
 
-  cbFn(common.mustCall((err, ret) => {
-    assert.ifError(err);
-    assert.strictEqual(ret, sentinel);
-  }));
+    const cbAsyncFn = callbackify(asyncFn);
+
+    cbAsyncFn(common.mustCall((err, ret) => {
+      assert.strictEqual(ret, undefined);
+      if (err instanceof Error) {
+        assert.strictEqual(String(sentinel).endsWith(err.message), true);
+        if ('cause' in err)
+          assert.strictEqual(err.cause, sentinel);
+      } else {
+        assert.strictEqual(err, sentinel);
+      }
+    }));
+  }
+
+  for (const sentinel of sentinalValues) {
+    function promiseFn() {
+      return Promise.reject(sentinel);
+    }
+
+    const cbPromiseFn = callbackify(promiseFn);
+
+    cbPromiseFn(common.mustCall((err, ret) => {
+      assert.strictEqual(ret, undefined);
+      if (err instanceof Error) {
+        assert.strictEqual(String(sentinel).endsWith(err.message), true);
+        if ('cause' in err)
+          assert.strictEqual(err.cause, sentinel);
+      } else {
+        assert.strictEqual(err, sentinel);
+      }
+    }));
+  }
 }
 
 {
-  async function fn3() {
-    return await Promise.reject(null);
-  }
-  const cbFn = callbackify(fn3);
+  // 3. args work
+  for (const sentinel of sentinalValues) {
+    async function asyncFn(arg) {
+      assert.strictEqual(arg, sentinel);
+      return await Promise.resolve(arg);
+    }
 
-  cbFn(common.mustCall((err, ret) => {
-    assert.strictEqual(Object.getPrototypeOf(err).name, 'Error');
-    assert.strictEqual(err.message, 'null');
-  }));
+    const cbAsyncFn = callbackify(asyncFn);
+
+    cbAsyncFn(sentinel, common.mustCall((err, ret) => {
+      assert.ifError(err);
+      assert.strictEqual(ret, sentinel);
+    }));
+
+    function promiseFn(arg) {
+      assert.strictEqual(arg, sentinel);
+      return Promise.resolve(arg);
+    }
+
+    const cbPromiseFn = callbackify(promiseFn);
+
+    cbPromiseFn(sentinel, common.mustCall((err, ret) => {
+      assert.ifError(err);
+      assert.strictEqual(ret, sentinel);
+    }));
+  }
 }
 
 {
+  // `this` binding
+  for (const sentinel of sentinalValues) {
+    const iAmThis = {
+      fn(arg) {
+        assert.strictEqual(this, iAmThis);
+        return Promise.resolve(arg);
+      },
+    };
+    iAmThis.cbFn = callbackify(iAmThis.fn);
+
+    iAmThis.cbFn(sentinel, common.mustCall(function(err, ret) {
+      assert.ifError(err);
+      assert.strictEqual(ret, sentinel);
+      assert.strictEqual(this, iAmThis);
+    }));
+
+    const iAmThat = {
+      async fn(arg) {
+        assert.strictEqual(this, iAmThat);
+        return await Promise.resolve(arg);
+      },
+    };
+    iAmThat.cbFn = callbackify(iAmThat.fn);
+
+    iAmThat.cbFn(sentinel, common.mustCall(function(err, ret) {
+      assert.ifError(err);
+      assert.strictEqual(ret, sentinel);
+      assert.strictEqual(this, iAmThat);
+    }));
+  }
+}
+
+{
+  // `uncaughtException` aborts process
   const fixture = join(fixtureDir, 'callbackify1.js');
   execFile(
     process.argv[0],
@@ -63,6 +165,7 @@ const fixtureDir = join(common.fixturesDir, 'uncaught-exceptions');
 }
 
 {
+  // handled `uncaughtException` works and passes rejection reason
   const fixture = join(fixtureDir, 'callbackify2.js');
   execFile(
     process.argv[0],
